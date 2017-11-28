d9cfbad234d3817f124cab6ed10751f2e150b7b6
Shishin Nikita <nktshn06@gmail.com>
2017-11-28 19:52:14 +0600

    refactoring

--- domains/client/index.php
+++ domains/client/index.php
@@ -1,6 +1,35 @@
 <?php
+
 require('db_conn.php');
 show_conn_status(is_connected());
+
+$table = $_POST['table-selector'];
+$query = 'SELECT * FROM ' . $table;
+$result = mysqli_query(is_connected(), $query);
+$out = mysqli_fetch_all($result);
+sort($out);
+
+$query = 'SHOW COLUMNS FROM ' . $table;
+$result = mysqli_query(is_connected(), $query);
+$t_headers = mysqli_fetch_all($result);
+
+function show_table($t_headers, $rows)
+{
+    echo "<tr>";
+    for ($i = 0; $i < count($t_headers); $i++) {
+        echo "<th class = 'table_th'>{$t_headers[$i][0]}</th>";
+    }
+
+    echo "</tr>";
+    for ($j = 0; $j < count($rows); $j++) {
+        echo "<tr>";
+        for ($i = 0; $i < count($t_headers); $i++) {
+            echo "<td class = 'table_th'>{$rows[$j][$i]}</td>";
+        }
+        echo "<tr>";
+    }
+}
+
 ?>
 <!doctype html>
 <html lang="en">
@@ -10,11 +39,11 @@ show_conn_status(is_connected());
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>THEATRE</title>
+    <title><?php echo strtoupper($table)?></title>
 </head>
 <body>
-<p>Выберите таблицу</p>
-<form action="/show-table.php" method="post">
+
+<form action="/index.php" method="post">
     <label for="table-selector"></label><select name="table-selector" id="table-selector">
         <option name="actors" value="actors">Актеры</option>
         <option name="contracts" value="contracts">Контракты</option>
@@ -24,5 +53,21 @@ show_conn_status(is_connected());
     </select>
     <button value="" type="submit">Показать</button>
 </form>
+
+
+<table class="table">
+    <?php
+    show_table($t_headers, $out);
+    ?>
+</table>
+
+
+<!--<pre>-->
+<?php
+//print_r($out);
+//?>
+<!--</pre>-->
 </body>
 </html>
+
+
--- domains/client/show-table.php
+++ domains/client/show-table.php
@@ -6,25 +6,27 @@ $table = $_POST['table-selector'];
 $query = 'SELECT * FROM ' . $table;
 $result = mysqli_query(is_connected(), $query);
 $out = mysqli_fetch_all($result);
+sort($out);
 
 $query = 'SHOW COLUMNS FROM ' . $table;
 $result = mysqli_query(is_connected(), $query);
 $t_headers = mysqli_fetch_all($result);
 
-function show_table($t_headers, $rows) {
- echo "<tr>";
-        for ($i = 0; $i < count($t_headers); $i++) {
-            echo "<th class = 'table_th'>{$t_headers[$i][0]}</th>";
-        }
-
- echo "</tr>";
-for ($j = 0; $j < count($rows); $j++) {
+function show_table($t_headers, $rows)
+{
     echo "<tr>";
     for ($i = 0; $i < count($t_headers); $i++) {
-        echo "<td class = 'table_th'>{$rows[$j][$i]}</td>";
+        echo "<th class = 'table_th'>{$t_headers[$i][0]}</th>";
     }
-    echo "<tr>";
-}
+
+    echo "</tr>";
+    for ($j = 0; $j < count($rows); $j++) {
+        echo "<tr>";
+        for ($i = 0; $i < count($t_headers); $i++) {
+            echo "<td class = 'table_th'>{$rows[$j][$i]}</td>";
+        }
+        echo "<tr>";
+    }
 }
 
 ?>
@@ -36,9 +38,22 @@ for ($j = 0; $j < count($rows); $j++) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>TABLE</title>
+    <title><?php echo strtoupper($table)?></title>
 </head>
 <body>
+
+<form action="/show-table.php" method="post">
+    <label for="table-selector"></label><select name="table-selector" id="table-selector">
+        <option name="actors" value="actors">Актеры</option>
+        <option name="contracts" value="contracts">Контракты</option>
+        <option name="play" value="play">Спектакли</option>
+        <option name="played" value="played">Отыграно</option>
+        <option name="role" value="role">Роли</option>
+    </select>
+    <button value="" type="submit">Показать</button>
+</form>
+
+
 <table class="table">
     <?php
     show_table($t_headers, $out);
